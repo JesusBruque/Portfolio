3052c61d7434da5fc6766936c6f9bafe59f34ebe
JesusBruque <bruke3_voleyutrera@hotmail.com>
2022-07-25 18:12:54 +0000

    commit 3

--- src/front/js/views/Home/Home.jsx
+++ src/front/js/views/Home/Home.jsx
@@ -4,9 +4,12 @@ import "./home.css";
 const Home = () => {
     return (
         <>
-            <img src="https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" />
-
-            <div> Hola!! Mi nombre es Jesús y soy desarrollador web full-stack.</div>
+            <div className="container">
+                <div className="presentation">
+                    <p className="p1">Hola!</p>
+                    <p className="p2">Mi nombre es Jesús y soy desarrollador full-stack</p>
+                </div>
+            </div>
         </>
     )
 }
